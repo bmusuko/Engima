178680adaccbb9171635518780ecffa5639bba2c
bmusuko <bram.musuko@gmail.com>
2019-11-19 19:20:20 +0700

    change default page

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 $cookieName = "user";
 
 if (empty($_COOKIE[$cookieName])) {
-	header("Location: http://localhost/engi_cinema/login.html");
+	header("Location: http://ec2-34-203-205-203.compute-1.amazonaws.com/engima/login.html");
 }
 else {
-	header("Location: http://localhost/engi_cinema/homepage.html");
+	header("Location: http://ec2-34-203-205-203.compute-1.amazonaws.com/engima/homepage.html");
 }
